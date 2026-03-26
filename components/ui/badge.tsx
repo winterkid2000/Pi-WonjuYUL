9ef29c0b9e81151a6a172644241f05e66a1dc28e
winterkid <137682088+winterkid2000@users.noreply.github.com>
2026-03-26 16:28:25 +0900

    Update badge.tsx

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -34,7 +34,7 @@ function Badge({
   ...props
 }: React.ComponentProps<"span"> &
   VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
-  const Comp = asChild ? Slot.Root : "span"
+  const Comp = asChild ? Slot : "span"
 
   return (
     <Comp
