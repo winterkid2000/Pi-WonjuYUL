1eb5b7846a9c64f2f518707064b5b0ba2758d199
winterkid <137682088+winterkid2000@users.noreply.github.com>
2026-03-26 15:26:33 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Pi-WonjuYUL",
+  description: "Pyramid imaging dashboard frontend demo",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
